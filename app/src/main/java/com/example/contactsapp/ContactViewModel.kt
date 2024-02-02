920e943947b5f5b5f9f420274d5166d1f7facc25
abdi <abdiakhmet.1.618@gmail.com>
2024-02-02 22:15:01 +0600

    Added functionality to viewModel (inserting contact)

--- app/src/main/java/com/example/contactsapp/ContactViewModel.kt
+++ app/src/main/java/com/example/contactsapp/ContactViewModel.kt
@@ -52,7 +52,28 @@ class ContactViewModel(
                 )}
                 }
             ContactEvent.SaveContact -> {
+                val firstName = state.value.firstName
+                val lastName = state.value.lastName
+                val phoneNumber = state.value.phoneNumber
 
+                if(firstName.isBlank() || lastName.isBlank() || phoneNumber.isBlank()) {
+                    return
+                }
+
+                val contact = Contact(
+                    firstName = firstName,
+                    lastName = lastName,
+                    phoneNumber = phoneNumber
+                )
+                viewModelScope.launch {
+                    dao.insertContact(contact)
+                }
+                _state.update {it.copy(
+                    isAddingContact = false,
+                    firstName = "",
+                    lastName = "",
+                    phoneNumber = ""
+                )}
             }
             is ContactEvent.SetFirstName -> {
                 _state.update {it.copy(
