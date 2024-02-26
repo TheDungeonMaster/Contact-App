6798c6bedc117adadcdd86ccd350a3a0e4576cf7
abdi <abdiakhmet.1.618@gmail.com>
2024-02-26 10:34:11 +0600

    test

--- app/src/main/java/com/example/contactsapp/ContactDatabase.kt
+++ app/src/main/java/com/example/contactsapp/ContactDatabase.kt
@@ -3,6 +3,7 @@ package com.example.contactsapp
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+
 @Database(
     entities = [Contact::class],
     version = 1
--- app/src/main/java/com/example/contactsapp/ContactScreen.kt
+++ app/src/main/java/com/example/contactsapp/ContactScreen.kt
@@ -64,7 +64,6 @@ fun ContactScreen(
                         }
                     }
                 }
-
             }
         }
 }
